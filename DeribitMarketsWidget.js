2fccd3338486a99bd3c460c714abe1e2c2017c0e
DarthChungo <antonio@adaro.eu>
2024-08-20 14:26:25 +0200

    add tenor, premium and apr calculations

--- DeribitMarketsWidget.js
+++ DeribitMarketsWidget.js
@@ -10,6 +10,10 @@ const API_URL = "https://www.deribit.com/api/v2/";
 // Widget Creation
 
 const fetchFutureInfo = async (currency) => {
+    const platform_time = await new Request(API_URL + "public/get_time")
+        .loadJSON()
+        .then((response) => response.result);
+
     const names = await new Request(API_URL + `public/get_instruments?currency=${currency}&expired=false&kind=future`)
         .loadJSON()
         .then((response) =>
@@ -19,26 +23,33 @@ const fetchFutureInfo = async (currency) => {
         );
 
     const futures = await Promise.all(
-        names.map(async (name) => (
-            {
+        names.map(async (name) => {
+            const info = {
                 ...await new Request(API_URL + `public/ticker?instrument_name=${name}`)
                     .loadJSON()
                     .then((response) => (
                         {
-                            name: name.split('-')[1],
                             price: response.result.mark_price,
-                            premium: (response.result.mark_price - response.result.index_price) / response.result.mark_price,
+                            premium: (response.result.mark_price - response.result.index_price) / response.result.index_price,
                         }
                     )),
                 ...await new Request(API_URL + `public/get_instrument?instrument_name=${name}`)
                     .loadJSON()
                     .then((response) => (
                         {
-                            expiration: response.result.settlement_period == "perpetual" ? null : response.result.expiration_timestamp,
+                            tenor: response.result.settlement_period == "perpetual" ? null : response.result.expiration_timestamp - platform_time,
                         }
                     )),
+            };
+
+            return {
+                name,
+                ...info,
+                tenor_string: Math.floor(info.tenor / (1000 * 60 * 60 * 24)) + "d" + Math.floor(info.tenor % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)) + "h",
+                premium_string: (info.premium * 100).toFixed(2) + "%",
+                apr_string: ((info.premium * 100 * 1000 * 60 * 60 * 24 * 365) / info.tenor).toFixed(2) + "%",
             }
-        ))
+        })
     );
 
     return futures;
